d41297348dae571d8177e06a5ef1cf7d30088cb6
wafflegolfing <wafflegolfing@gmail.com>
2014-08-22 01:43:47 +0200

    ui

--- app/services/geoLocate.js
+++ app/services/geoLocate.js
@@ -5,6 +5,10 @@ angular.module('geoLocateFactory',
     if (!navigator.geolocation) {
         return cb('no geolocate');
     }
+    if (navigator.userAgent && navigator.userAgent.toLowerCase().indexOf('firefox') !== -1) {
+        return cb('firefox');
+    }
+
     navigator.geolocation.getCurrentPosition(function (pos) {
       var latitude, longitude, geocoder, latlng;
 
